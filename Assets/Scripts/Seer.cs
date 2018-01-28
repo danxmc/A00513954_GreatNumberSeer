595c761a77c832b5285c29b71d4b87ec4336cd13
danxmc <danx_97@hotmail.com>
2018-01-28 02:53:11 -0600

    Add game assets

---
+++ Assets/Scripts/Seer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Seer : MonoBehaviour {
+    //Attributes
+    private int min;
+    private int max;
+    private int guess;
+    public int attempts;
+    public Text guessLabel;
+    LevelManager levelManager;
+
+    // Use this for initialization
+    void Start () {
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+        StartGame();
+        ShowGuess();
+	}
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void StartGame()
+    {
+        min = 1;
+        max = 1001;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = UnityEngine.Random.Range(min, max);
+        attempts--;
+    }
+
+    void ShowGuess()
+    {
+        if (attempts >= 0)
+        {
+            guessLabel.text = "Is your number " + guess + "?\n\nAttempts : " + attempts;
+        } else
+        {
+            levelManager.LoadLevel("win");
+        }
+    }
+
+    public void GuessHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+        ShowGuess();
+    }
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+        ShowGuess();
+    }
+
+    public void CorrectGuess()
+    {
+        levelManager.LoadLevel("lost");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # A00513954_GreatNumberSeer
+
+Video Game Development Project
+Game 02
